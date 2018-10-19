7c43d18e42aa44c6c33415943574194c82293418
Aliot26 <olgakochanowa@gmail.com>
2018-10-19 19:53:06 +0200

    Initial commit

---
+++ gameInventory.py
@@ -0,0 +1,106 @@
+import operator
+# This is the file where you must work. Write code in the functions, create new functions,
+# so they work according to the specification
+
+"""inv = {'arrow': 12, 'gold coin': 42, 'rope': 1, 'torch': 6, 'dagger': 1}
+dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']"""
+# Displays the inventory.
+
+
+def display_inventory(inventory):
+    print(" Inventory:")
+    sumItems = 0
+    for key in inventory.keys():
+        print("%s   %s" % (inventory[key], key))
+        sumItems += inventory[key]
+    print("Total number of items: ", sumItems)
+
+
+# Adds to the inventory dictionary a list of items from added_items.
+
+
+def add_to_inventory(inventory, added_items):
+    for lootItem in added_items:
+        if lootItem in inventory:
+            inventory[lootItem] += 1
+        else:
+            inventory.setdefault(lootItem, 1)
+    return inventory
+
+    # Takes your inventory and displays it in a well-organized table with
+    # each column right-justified. The input argument is an order parameter (string)
+    # which works as the following:
+    # - None (by default) means the table is unordered
+    # - "count,desc" means the table is ordered by count (of items in the inventory)
+    #   in descending order
+    # - "count,asc" means the table is ordered by count in ascending order
+
+
+def print_table(inventory, order=None):
+    if order == "count,desc":
+        listSorted = [(k, inventory[k])
+                      for k in sorted(inventory, key=inventory.get, reverse=True)]
+        function_print(listSorted)
+    elif order == "count,asc":
+        listSorted = [(k, inventory[k])
+                      for k in sorted(inventory, key=inventory.get, reverse=False)]
+        function_print(listSorted)
+    else:
+        listNotSorted = inventory.items()
+        function_print(listNotSorted)
+
+
+def function_print(invList):
+    hi = max(invList, key=len)
+    maxLenStr = len(hi[0])
+    a = 17
+    print("Inventory: ")
+    print("-"*(a+maxLenStr))
+    print('{:>7}{:>{lenght}}'.format(
+        "count", "item name", lenght=maxLenStr+9))
+    print("-"*(a+maxLenStr))
+    sumItems = 0
+    for x in invList:
+        print('{:>7}{:>{lenght}}'.format(
+            x[1], x[0], lenght=maxLenStr+9))
+        sumItems += x[1]
+    print("-"*(a+maxLenStr))
+    print("Total number of items: ", sumItems)
+
+# Imports new inventory items from a file
+# The filename comes as an argument, but by default it's
+# "import_inventory.csv". The import automatically merges items by name.
+# The file format is plain text with comma separated values (CSV).
+
+
+def import_inventory(inventory, filename="import_inventory.csv"):
+    new_inv = open("import_inventory.csv")
+    newInv = new_inv.read()
+    new_inv.close()
+    newInv = newInv.split(',')
+    newInv = add_to_inventory(inventory, newInv)
+    print_table(newInv, order=None)
+
+
+# Exports the inventory into a .csv file.
+# if the filename argument is None it creates and overwrites a file
+# called "export_inventory.csv". The file format is the same plain text
+# with comma separated values (CSV).
+def export_inventory(inventory, filename="export_inventory.csv"):
+    new_inv = open("export_inventory.csv", "w")
+    inventory = list(inventory)
+    for item in inventory:
+        new_inv.write(item + ", ")
+    size = new_inv.tell()
+    new_inv.truncate(size - 2)
+    new_inv.close()
+
+
+# Don't forget to delete this
+
+"""display_inventory(inv)
+inv = add_to_inventory(inv, dragon_loot)
+display_inventory(inv)
+print_table(inv)
+import_inventory(inv)
+export_inventory(inv)"""
